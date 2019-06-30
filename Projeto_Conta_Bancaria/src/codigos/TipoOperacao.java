73922f94ef5e0623c01e08ebc5033671829a7c7c
Ayrton Saué Cossuol <43251314+Ayrton-Cossuol@users.noreply.github.com>
2019-06-30 20:32:48 -0300

    Update TipoOperacao.java

--- Projeto_Conta_Bancaria/src/codigos/TipoOperacao.java
+++ Projeto_Conta_Bancaria/src/codigos/TipoOperacao.java
@@ -1,5 +1,19 @@
 package codigos;
+/**
+ * Classe Enum utilizada para fornecer mais precisao ao acesso dos tipos de operações 
+ * em que o Usuario irá ter.
+ * 
+ * @author Arthur B. 
+ * @author Ayrton S. C.
+ * @author Diogenes F.
+ * @author Luan S. F
+ * @author Yuri C. P.
+ * @version 1.0 (junho - 2019)
+ */
 public enum TipoOperacao {
+	/**
+	 * Os tipos de Operações sao utilizadas do mesmo modo que estão descritas abaixo
+	 */
 	SAQUE, DEPOSITO, RECEBEU_TRANSFERENCIA, REALIZOU_TRANSFERENCIA;
 }
 
